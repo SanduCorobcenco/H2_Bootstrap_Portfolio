d0011967a4c96a1b08d0b4b8fd4964f639170b8d
Sandu Corobcenco <corobcenco.sandu@gmail.com>
2020-12-06 14:51:50 -0800

    Prework for bootcamp

---
+++ Desktop/PREWORK_SC/Module-6/javascript.js
@@ -0,0 +1,33 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor= "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity= "0.5";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.boxShadow="5px 10px #888888";
+
+});
+
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px; ";
+
+});
+
+
+
